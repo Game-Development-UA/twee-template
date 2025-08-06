6b09cbb2b66428a0477698ce9ac30a3640aee84c
dcastalia <drewc@hwstn.com>
2025-08-06 13:07:27 -0700

    Updating template to use a codespace-friendly compilation pipeline with default settings.

--- compile_for_Windows.bat
+++
@@ -1,6 +0,0 @@
-cd "%~dp0"compiler\tweego-2.1.1-windows-x64
-set "OUTPUT_PATH=%~dp0simulation.html"
-set "INPUT_PATH=%~dp0source"
-tweego.exe -o "%OUTPUT_PATH%" "%INPUT_PATH%"
-ECHO "Compilation complete.  Your generated game can be found at %OUTPUT_PATH%"
-PAUSE
--- source/scripts.js
+++
@@ -1,7 +0,0 @@
-/* Stow the sidebar initially
-config.ui.stowBarInitially = true;
-*/
-
-/* Hide the back / forward buttons
-config.history.controls = false;
-*/
